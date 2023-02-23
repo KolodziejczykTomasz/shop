1b3b5accb6609e6467f1c3aa45b4ef001ec92de9
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2023-02-23 12:52:08 +0100

    Changed content in page edit view.

--- pages/products/edit/[id].vue
+++ pages/products/edit/[id].vue
@@ -2,53 +2,85 @@
     <Header />
     <section>
         <div id="product__edit">
-            <div class="product__edit__wrapper">
-                <div class="product__edit__details-photo">
-                    <img :src="`${product.image}`" :alt="`${product.title}`" />
-                </div>
-                <div class="product__edit__details">
-                    <p class="product__edit__details-name">{{ product.title }}</p>
-                    <p class="product__edit__details-price">{{ product.price }}zł</p>                  
-                    <p class="product__edit__details-availability">Dostępność: {{ product.availability }} sztuk</p>
+            <div class="product__edit__wrapper">  
+                <form @submit.prevent="hanldeEditProduct()">            
+                    <input 
+                        type="text" 
+                        name="image" 
+                        v-model="product.image"                               
+                        placeholder="Adres url"
+                    />   
+                    <input 
+                        type="text" 
+                        name="title" 
+                        v-model="product.title" 
+                        placeholder="Nazwa"
+                    />              
+                   <input 
+                        type="text" 
+                        name="price" 
+                        v-model="product.price" 
+                        placeholder="Cena [zł]"
+                    />               
+                    <input 
+                        type="text" 
+                        name="availability" 
+                        v-model="product.availability"  
+                        placeholder="Dostępność [szt.]"
+                     />
+                    <textarea 
+                        type="textarea"
+                        name="description" 
+                        v-model="product.description" 
+                        placeholder="Opis">
+                    </textarea>   
                     
-                </div>
+                <div class="product__edit__details-edit">               
+                    <NuxtLink to="/shop">    
+                    <button class="product__edit__details-button">Anuluj</button>
+                    </NuxtLink>
+                    <NuxtLink to="">    
+                    <button class="product__edit__details-button" :disabled="isSaving"> 
+                        <span v-show="!isSaving">Zapisz</span>
+                        <span v-show="isSaving">zapisywanie...</span></button>
+                    </NuxtLink>            
+                </div> 
+            </form>         
             </div>
-            <div class="product__edit__details-description">
-                {{ product.description }}
-            </div>
-            <div class="product__edit__details-edit">               
-                <NuxtLink to="/shop">    
-                <button class="product__edit__details-button">Anuluj</button>
-                </NuxtLink>
-                <NuxtLink to="">    
-                <button class="product__edit__details-button" :disabled="isPending"> 
-                    <span v-show="!isPending">Zapisz</span>
-                    <span v-show="isPending">zapisywanie...</span></button>
-                </NuxtLink>
-            
-            </div>          
         </div>
     </section>   
     <Footer />
 </template>
-<script setup >
+<script setup lang="ts">
     import Header from "../../../components/header.vue";
     import Footer from "../../../components/footer.vue";
     import { useCartStore } from "~~/stores/cartStore";
-
+    
+    const isSaving = ref(false)
     const cartStore = useCartStore()
     cartStore.getCart();
 
     const { id } = useRoute().params
     const uri = 'http://localhost:4000/products/' + id
-    const { data: product } = await useFetch(uri, { key: id })
+    const { data: product } = await useFetch(uri, { key: "id" })
 
-    
-    const addToBasket = async () => {
-        isPending.value = true
-        await cartStore.addToCart(product)
+interface productToChange {
+    image: string,
+    title: string,
+    price: string,
+    availability: string,
+    description: string
+}
+   const productToChange: productToChange = { image: "", title: "", price: "", availability: "", description: ""  };
+
+
+const emit = defineEmits(['update:modelValue'])
+
+    const hanldeEditProduct = async () => {
+        isSaving.value = true
+        console.log(productToChange)
         setTimeout(() => {
-            isPending.value = false
+            isSaving.value = false
         }, 3000)
     }
 
@@ -61,90 +93,16 @@ useHead({
 
 <style lang="scss" scoped>
 #product__edit {
-    margin: 50px 0 10px 0;   
+    margin: 50px 0 50px 0;   
     height: auto;
     width: 100%;
-}
-
-.product__edit__details-description {
-    min-height: 100px;
-    height: auto;
-    width: 100%;
-    font-size: $font-size-m;
-    line-height: 24px;
-    margin-top: 40px;
-    padding: 0 5px;
 }
 
 .product__edit__wrapper {
     display: flex;
-    flex-direction: row;
-
-    & .product__edit__details-photo {
-        @include flex-center;
-        width: 100%;
-        border: 1px solid $primary-color-green;
-
-        & img {
-            @include flex-center;
-            max-height: 320px;
-            max-width: 320px;
-            width: 100%;
-            height: 100%;           
-        }
-    }
-
-    & .product__edit__details {
-        display: flex;
-        flex-direction: column;
-        margin-left: 35px;
-        width: 100%;
-        
-    }
+    flex-direction: column;
 }
 
-@media only screen and (min-width: 768px) {
-    .product__edit__wrapper {
-        flex-direction: row;
-    }
-
-    .product__edit__details-photo {
-        max-width: 500px;
-        width: auto;
-    }
-}
-
-.product__edit__details-name {
-    font-size: $font-size-xl;
-    padding-top: 20px;
-    margin-bottom: 20px;
-    font-weight: $bold;
-}
-
-.product__edit__details-price,
-.product__edit__details-amount,
-.product__edit__details {
-    font-size: $font-size-m;
-    font-weight: normal;
-    margin-top: 15px;
-    margin-bottom: 15px;
-}
-
-.product__edit__details-amount {
-    display: flex;
-    justify-content: flex-start;
-}
-
-.product__edit__details-availability {
-    margin-top: 10px;
-    margin-left: -5px;
-}
-
-.product__edit__details-edit {
-    display: flex;
-    justify-content: flex-end;
-    margin: 0 20px 70px 0;
-}
 
 .product__edit__details-button {
     @include flex-center;
@@ -166,5 +124,39 @@ useHead({
         } 
 }
 
+.product__edit__details-edit {
+    display: flex;    
+    flex-direction: row;
+}
 
+.product__edit__wrapper form {
+    @include flex-center;
+    flex-direction: column;
+    & input {
+        display: flex;
+        min-width: 300px;
+        width: 60%;
+        height: 40px;
+        margin: 15px 0;
+        border-radius: 20px;
+        padding-left:20px;
+        &:focus {
+            border: 2px solid $primary-color-green !important;
+        }
+    }
+
+    & textarea {
+        display: flex;
+        min-width: 300px;
+        width: 60%;
+        height: 120px;
+        margin: 20px 0;
+        border-radius: 20px;
+        padding-left:20px;
+        padding-top:10px;
+        &:focus {
+            border: 2px solid $primary-color-green !important;
+        }
+    }
+}
 </style>
